ad607d9e74cd843eaca2f531b8b6d47197babff0
Ryan-Willpower <surasak.c.work@gmail.com>
2019-10-20 23:19:00 +0700

    [testing] add login unit test

--- __mocks__/pg.js
+++ __mocks__/pg.js
@@ -2,7 +2,15 @@ const pg = jest.genMockFromModule('pg')
 
 class Pool {
   async query() {
-    return 'success'
+    return {
+      rowCount: 2,
+      rows: [
+        {
+          passwd:
+            '$2b$10$9qHrymuVGUdweHLDRc/R7.msa/4.9OXujRAbxy8QJWzh0izsanGBS',
+        },
+      ],
+    }
   }
 }
 
